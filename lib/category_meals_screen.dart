115d1dea0ae940aae4eded5caa5c94b7ab3765f0
shogil <shogilbabu@gmail.com>
2022-06-30 20:12:44 +0530

    update the path

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -6,9 +6,13 @@ class CategoryMealsScreen extends StatelessWidget {
   // CategoryMealsScreen(this.CategoryId, this.CategoryTitle);
   @override
   Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context)!.settings.arguments as Map<String, String>;
+    final categoryTitle = routeArgs['title'];
+    final categoryId = routeArgs['id'];
     return Scaffold(
       appBar: AppBar(
-        title: Text(CategoryTitle),
+        title: Text(categoryTitle!),
       ),
       body: Center(
         child: Text(
